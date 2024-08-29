7fcde4a5cfd2ead88f5436a4d39519d4f7b0ee09
devodii <emmanuelodii80@gmail.com>
2024-08-29 08:27:12 +0100

    feat: introduce custom fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Asap forms</div>;
+  return (
+    <div className="h-screen w-screen flex items-center justify-center font-lato">
+      <h2 className="text-2xl font-semibold">Asap forms</h2>
+    </div>
+  );
 }
