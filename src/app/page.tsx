f77aa7afda17f57bb6595425a49062bc9176c164
devodii <emmanuelodii80@gmail.com>
2024-09-11 00:30:52 +0100

    feat(fonts): use system fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col gap-2 items-center justify-center font-lato bg-gray-50">
-      <h2 className="text-2xl font-semibold">Asap Forms</h2>
+      <b className="text-2xl font-medium">Asap Forms</b>
       <p className="text-foreground">The Open source alternative for Jotform</p>
     </div>
   );
