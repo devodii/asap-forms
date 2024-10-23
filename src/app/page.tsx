39e1a898c48f8b780325812c981d2d6363ddc3b4
devodii <emmanuelodii80@gmail.com>
2024-10-23 19:08:39 +0100

    feat: improve main landing page copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import { Waitlist } from "@/components/waitlist"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-2 bg-gray-50">
-      <b className="text-3xl font-medium">Asap Forms</b>
-      <p className="text-xl text-foreground">
-        Get feedback from your customers in less than a minute
-      </p>
-      <Waitlist />
+      <div className="container mx-auto flex flex-col items-center justify-center gap-4 px-24 py-12">
+        <h4 className="text-center text-4xl font-semibold">
+          Build better forms
+        </h4>
+        <p className="text-md flex text-center text-secondary-foreground">
+          Recieve feedback from your users in less than one minute
+        </p>
+
+        <Button asChild className="mx-auto w-full max-w-xl">
+          <Link href="/sign-up">Start</Link>
+        </Button>
+      </div>
     </div>
   )
 }
